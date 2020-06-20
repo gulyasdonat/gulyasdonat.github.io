f3893cb24f84c0d8ea608980cb1f0cd4c744df47
unknown <gulyas.donat@gmail.com>
2020-06-20 17:23:20 +0200

    v0.12 added epites js

---
+++ epites-bontas.js
@@ -0,0 +1,80 @@
+function fohadiszallasepites() {
+    var epit = document.getElementById('fohadiszallas-e-b').innerHTML;
+    epit++;
+    document.getElementById('fohadiszallas-e-b').innerHTML=epit;
+}
+function barakkepites() {
+    var epit = document.getElementById('barakk-e-b').innerHTML;
+    epit++;
+    document.getElementById('barakk-e-b').innerHTML=epit;
+}
+function istalloepites() {
+    var epit = document.getElementById('istallo-e-b').innerHTML;
+    epit++;
+    document.getElementById('istallo-e-b').innerHTML=epit;
+}
+function muhelyepites() {
+    var epit = document.getElementById('muhely-e-b').innerHTML;
+    epit++;
+    document.getElementById('muhely-e-b').innerHTML=epit;
+}
+function akademiaepites() {
+    var epit = document.getElementById('akademia-e-b').innerHTML;
+    epit++;
+    document.getElementById('akademia-e-b').innerHTML=epit;
+}
+function kovacsmuhelyepites() {
+    var epit = document.getElementById('kovacsmuhely-e-b').innerHTML;
+    epit++;
+    document.getElementById('kovacsmuhely-e-b').innerHTML=epit;
+}
+function gyulekezohelyepites() {
+    var epit = document.getElementById('gyulekezohely-e-b').innerHTML;
+    epit++;
+    document.getElementById('gyulekezohely-e-b').innerHTML=epit;
+}
+function szoborepites() {
+    var epit = document.getElementById('szobor-e-b').innerHTML;
+    epit++;
+    document.getElementById('szobor-e-b').innerHTML=epit;
+}
+function piacepites() {
+    var epit = document.getElementById('piac-e-b').innerHTML;
+    epit++;
+    document.getElementById('piac-e-b').innerHTML=epit;
+}
+function fatelepepites() {
+    var epit = document.getElementById('fatelep-e-b').innerHTML;
+    epit++;
+    document.getElementById('fatelep-e-b').innerHTML=epit;
+}
+function agyagbanyaepites() {
+    var epit = document.getElementById('agyagbanya-e-b').innerHTML;
+    epit++;
+    document.getElementById('agyagbanya-e-b').innerHTML=epit;
+}
+function vasbanyaepites() {
+    var epit = document.getElementById('vasbanya-e-b').innerHTML;
+    epit++;
+    document.getElementById('vasbanya-e-b').innerHTML=epit;
+}
+function tanyaepites() {
+    var epit = document.getElementById('tanya-e-b').innerHTML;
+    epit++;
+    document.getElementById('tanya-e-b').innerHTML=epit;
+}
+function raktarepites() {
+    var epit = document.getElementById('raktar-e-b').innerHTML;
+    epit++;
+    document.getElementById('raktar-e-b').innerHTML=epit;
+}
+function rejtekhelyepites() {
+    var epit = document.getElementById('rejtekhely-e-b').innerHTML;
+    epit++;
+    document.getElementById('rejtekhely-e-b').innerHTML=epit;
+}
+function falepites() {
+    var epit = document.getElementById('fal-e-b').innerHTML;
+    epit++;
+    document.getElementById('fal-e-b').innerHTML=epit;
+}
