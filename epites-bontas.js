4ada80b7451ad1f13725a75ac702f989746e95fc
unknown <gulyas.donat@gmail.com>
2020-06-20 17:46:03 +0200

    v0.12a added max epites number in epites-bontas.js

--- epites-bontas.js
+++ epites-bontas.js
@@ -1,80 +1,96 @@
 function fohadiszallasepites() {
     var epit = document.getElementById('fohadiszallas-e-b').innerHTML;
     epit++;
-    document.getElementById('fohadiszallas-e-b').innerHTML=epit;
+    epit2=Math.min(epit,30);
+    document.getElementById('fohadiszallas-e-b').innerHTML=epit2;
 }
 function barakkepites() {
     var epit = document.getElementById('barakk-e-b').innerHTML;
     epit++;
-    document.getElementById('barakk-e-b').innerHTML=epit;
+    epit2=Math.min(epit,25);
+    document.getElementById('barakk-e-b').innerHTML=epit2;
 }
 function istalloepites() {
     var epit = document.getElementById('istallo-e-b').innerHTML;
     epit++;
-    document.getElementById('istallo-e-b').innerHTML=epit;
+    epit2=Math.min(epit,20);
+    document.getElementById('istallo-e-b').innerHTML=epit2;
 }
 function muhelyepites() {
     var epit = document.getElementById('muhely-e-b').innerHTML;
     epit++;
-    document.getElementById('muhely-e-b').innerHTML=epit;
+    epit2=Math.min(epit,15);
+    document.getElementById('muhely-e-b').innerHTML=epit2;
 }
 function akademiaepites() {
     var epit = document.getElementById('akademia-e-b').innerHTML;
     epit++;
-    document.getElementById('akademia-e-b').innerHTML=epit;
+    epit2=Math.min(epit,1);
+    document.getElementById('akademia-e-b').innerHTML=epit2;
 }
 function kovacsmuhelyepites() {
     var epit = document.getElementById('kovacsmuhely-e-b').innerHTML;
     epit++;
-    document.getElementById('kovacsmuhely-e-b').innerHTML=epit;
+    epit2=Math.min(epit,20);
+    document.getElementById('kovacsmuhely-e-b').innerHTML=epit2;
 }
 function gyulekezohelyepites() {
     var epit = document.getElementById('gyulekezohely-e-b').innerHTML;
     epit++;
-    document.getElementById('gyulekezohely-e-b').innerHTML=epit;
+    epit2=Math.min(epit,1);
+    document.getElementById('gyulekezohely-e-b').innerHTML=epit2;
 }
 function szoborepites() {
     var epit = document.getElementById('szobor-e-b').innerHTML;
     epit++;
-    document.getElementById('szobor-e-b').innerHTML=epit;
+    epit2=Math.min(epit,1);
+    document.getElementById('szobor-e-b').innerHTML=epit2;
 }
 function piacepites() {
     var epit = document.getElementById('piac-e-b').innerHTML;
     epit++;
-    document.getElementById('piac-e-b').innerHTML=epit;
+    epit2=Math.min(epit,30);
+    document.getElementById('piac-e-b').innerHTML=epit2;
 }
 function fatelepepites() {
     var epit = document.getElementById('fatelep-e-b').innerHTML;
     epit++;
-    document.getElementById('fatelep-e-b').innerHTML=epit;
+    epit2=Math.min(epit,30);
+    document.getElementById('fatelep-e-b').innerHTML=epit2;
 }
 function agyagbanyaepites() {
     var epit = document.getElementById('agyagbanya-e-b').innerHTML;
     epit++;
-    document.getElementById('agyagbanya-e-b').innerHTML=epit;
+    epit2=Math.min(epit,30);
+    document.getElementById('agyagbanya-e-b').innerHTML=epit2;
 }
 function vasbanyaepites() {
     var epit = document.getElementById('vasbanya-e-b').innerHTML;
     epit++;
-    document.getElementById('vasbanya-e-b').innerHTML=epit;
+    epit2=Math.min(epit,30);
+    document.getElementById('vasbanya-e-b').innerHTML=epit2;
 }
 function tanyaepites() {
     var epit = document.getElementById('tanya-e-b').innerHTML;
     epit++;
-    document.getElementById('tanya-e-b').innerHTML=epit;
+    epit2=Math.min(epit,30);
+    document.getElementById('tanya-e-b').innerHTML=epit2;
 }
 function raktarepites() {
     var epit = document.getElementById('raktar-e-b').innerHTML;
     epit++;
-    document.getElementById('raktar-e-b').innerHTML=epit;
+    epit2=Math.min(epit,30);
+    document.getElementById('raktar-e-b').innerHTML=epit2;
 }
 function rejtekhelyepites() {
     var epit = document.getElementById('rejtekhely-e-b').innerHTML;
     epit++;
-    document.getElementById('rejtekhely-e-b').innerHTML=epit;
+    epit2=Math.min(epit,10);
+    document.getElementById('rejtekhely-e-b').innerHTML=epit2;
 }
 function falepites() {
     var epit = document.getElementById('fal-e-b').innerHTML;
     epit++;
-    document.getElementById('fal-e-b').innerHTML=epit;
+    epit2=Math.min(epit,20);
+    document.getElementById('fal-e-b').innerHTML=epit2;
 }
