811584a47575e711390eb7d102125d06b68ec120
unknown <gulyas.donat@gmail.com>
2020-06-21 18:59:04 +0200

    v0.14 working pontszamitas

--- js/pontok.js
+++ js/pontok.js
@@ -1,13 +1,12 @@
 function pontszamitas() {
-    var helyezes = 0;
+    var helyezes = 1;
     var ponts = 0;
-    var fohadiszallaslevel =Number(document.getElementById('fohadiszallas-e-b').innerHTML);
+    var fohadiszallaslevel = Number(document.getElementById('fohadiszallas-e-b').innerHTML);
     var barakklevel = Number(document.getElementById('barakk-e-b').innerHTML);
     var istallolevel = Number(document.getElementById('istallo-e-b').innerHTML);
     var muhelylevel = Number(document.getElementById('muhely-e-b').innerHTML);
     var akademialevel = Number(document.getElementById('akademia-e-b').innerHTML);
     var kovacsmuhelylevel = Number(document.getElementById('kovacsmuhely-e-b').innerHTML);
-    var gyulekezohelylevel = Number(document.getElementById('gyulekezohely-e-b').innerHTML);
     var szoborlevel = Number(document.getElementById('szobor-e-b').innerHTML);
     var piaclevel = Number(document.getElementById('piac-e-b').innerHTML);
     var fateleplevel = Number(document.getElementById('fatelep-e-b').innerHTML);
@@ -17,12 +16,54 @@ function pontszamitas() {
     var raktarlevel = Number(document.getElementById('raktar-e-b').innerHTML);
     var rejtekhelylevel = Number(document.getElementById('rejtekhely-e-b').innerHTML);
     var fallevel = Number(document.getElementById('fal-e-b').innerHTML);
-    
-    /*if (fohadiszallaslevel >= 0) {
-        ponts = fohadiszallaslevel*100;
-    };*/
 
-    ponts=fohadiszallaslevel+barakklevel+istallolevel;
-    document.getElementById('ranglista-pontok').innerHTML=
-    "Ranglista(" + helyezes + ".|" + ponts + " P)";
+
+    if (fohadiszallaslevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, fohadiszallaslevel - 1) * 10);   
+    }
+    if (barakklevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, barakklevel - 1) * 16);
+    }
+    if (istallolevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, istallolevel - 1) * 20);
+    }
+    if (muhelylevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, muhelylevel - 1) * 24);
+    }
+    if (akademialevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, akademialevel - 1) * 512);
+    }
+    if (kovacsmuhelylevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, kovacsmuhelylevel - 1) * 19);
+    }
+    if (szoborlevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, szoborlevel - 1) * 24);
+    }
+    if (piaclevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, piaclevel - 1) * 10);
+    }
+    if (fateleplevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, fateleplevel - 1) * 6);
+    }
+    if (agyagbanyalevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, agyagbanyalevel - 1) * 6);
+    }
+    if (vasbanyalevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, vasbanyalevel - 1) * 6);
+    }
+    if (tanyalevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, tanyalevel - 1) * 5);
+    }
+    if (raktarlevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, raktarlevel - 1) * 6);
+    }
+    if (rejtekhelylevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, rejtekhelylevel - 1) * 5);
+    }
+    if (fallevel>0) {
+        ponts = ponts + Math.round(Math.pow(1.2, fallevel - 1) * 8);
+    }   
+
+    document.getElementById('ranglista-pontok').innerHTML =
+        "Ranglista(" + helyezes + ".|" + ponts + " P)";
 }
